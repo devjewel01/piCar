2de8b79a14a83d9b429468d7afe8fd11600d3fe7
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-05-03 12:34:16 +0600

    update virtual car

--- virtualCar/main.py
+++ virtualCar/main.py
@@ -1,64 +1,138 @@
-from motor import Forward, Backward, Left, Right, Stop
+
+
+from motor import Forward, Backward, Right, Left, Stop
 import pygame
+from time import sleep
+
+
+# initialize things
 pygame.init()
-window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('./image/track6.png')
-car = pygame.image.load('./image/tesla.png')
-car = pygame.transform.scale(car, (30, 60))
-car_x = 155
-car_y = 300
-focal_dis = 25
-cam_x_offset = 0
-cam_y_offset = 0
-direction = 'up'
-drive = True
+robot = serial.Serial(PORT, BUADRATE)  
+
+
+window = pygame.display.set_mode((700,400))  
+#window = pygame.display.set_mode((1155,399))   
+
+
+bg  = pygame.image.load("image/track1.png")
+#bg  = pygame.image.load("image/track2.png")
+car = pygame.image.load("car.png")
+car = pygame.transform.scale(car, (40, 40)) 
+
+
 clock = pygame.time.Clock()
-while drive:
+
+
+car_x = 30   
+car_y = 260  
+
+JUMP_VALUE = 25     # turning point value
+direction = 'y_up'  # cars current direction
+run = 1
+
+
+Forward()
+DELAY = .400
+# main loop
+while run:
+    clock.tick(30)        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            drive = False
-    clock.tick(60)
-    cam_x = car_x + cam_x_offset + 15
-    cam_y = car_y + cam_y_offset + 15
-    up_px = window.get_at((cam_x, cam_y - focal_dis))[0]
-    down_px = window.get_at((cam_x, cam_y + focal_dis))[0]
-    right_px = window.get_at((cam_x + focal_dis, cam_y))[0]
-    print(up_px, right_px, down_px)
+            run = 0
 
-    # change direction (take turn)
-    if direction == 'up' and up_px != 255 and right_px == 255:
-        direction = 'right'
-        cam_x_offset = 30
-        car = pygame.transform.rotate(car, -90)
-    elif direction == 'right' and right_px != 255 and down_px == 255:
-        direction = 'down'
-        car_x = car_x + 30
-        cam_x_offset = 0
-        cam_y_offset = 30
-        car = pygame.transform.rotate(car, -90)
-    elif direction == 'down' and down_px != 255 and right_px == 255:
-        direction = 'right'
-        car_y = car_y + 30
-        cam_x_offset = 30
-        cam_y_offset = 0
-        car = pygame.transform.rotate(car, 90)
-    elif direction == 'right' and right_px != 255 and up_px == 255:
-        direction = 'up'
-        car_x = car_x + 30
-        cam_x_offset = 0
-        car = pygame.transform.rotate(car, 90)
-    # drive
-    if direction == 'up' and up_px == 255:
-        Forward()
-        car_y = car_y - 2
-    elif direction == 'right' and right_px == 255:
-        Right()
-        car_x = car_x + 2
-    elif direction == 'down' and down_px == 255:
-        Left()
-        car_y = car_y + 2
+    # position images
+    window.blit(bg, (0, 0))          # load the track image
+    window.blit(car, (car_x, car_y)) # the car image
+
+
+    last_x, last_y = car_x, car_y
+    
+    # find the center of the car and draw a point on that
+    center_x, center_y = (int(car_x + 40 /2), int(car_y + 40 / 2))
+    pygame.draw.circle(window, (0,255,255), (center_x, center_y), 5, 5)
+
+   
+    cal_value = 30             
+    y_up      = window.get_at((center_x, center_y - cal_value))[0]
+    y_down    = window.get_at((center_x, center_y + cal_value))[0]
+    x_right   = window.get_at((center_x + cal_value, center_y))[0]
+    x_left    = window.get_at((center_x - cal_value, center_y))[0]
+    #print("y_up   ", y_up)
+    #print("y_down ", y_down)
+    #print("x_right", x_right)
+    #print("x_left ", x_left)
+    #print("-----------")
+
+    # determine which way to go
+    # go up
+    if y_up == 255 and direction == 'y_up' and x_left != 255 and x_right != 255:
+        # move up
+        car_y -= 2  # decrease pixel and move the car on y axis
         
-    window.blit(track, (0, 0))
-    window.blit(car, (car_x, car_y))
-    pygame.draw.circle(window, (0, 255, 0), (cam_x, cam_y), 5, 5)
-    pygame.display.update()
+    # make the turn
+    if y_up == 255 and direction == 'y_up' and x_left != 255 and x_right == 255:
+        # make a right turn
+        direction = 'x_right'
+        car_y -= JUMP_VALUE
+        car_x += JUMP_VALUE
+        car = pygame.transform.rotate(car, -90)
+        window.blit(car, (car_x, car_y))
+        print('Turn Right')
+        Right()
+        sleep(DELAY)
+        Forward()
+
+    # go x right
+    if y_up != 255 and direction == 'x_right' and y_down != 255 and x_right == 255:
+        car_x += 2
+
+    if y_down == 255 and direction == 'x_right' and x_left == 255 and x_right == 255:
+        # make a turn from x_right
+        car = pygame.transform.rotate(car, -90)
+        direction = 'y_down'
+        car_y += JUMP_VALUE + 5
+        car_x += JUMP_VALUE
+        window.blit(car, (car_x, car_y))
+        print('Turn Right')
+        Right()
+        sleep(DELAY)
+        Forward()
+
+    # go y down
+    if y_down == 255 and direction == 'y_down' and x_left != 255 and x_right != 255:
+        # move down
+        car_y += 2
+
+    # left turn
+    if y_down == 255 and direction == 'y_down' and x_left != 255 and x_right == 255:
+        # turn from y_down
+        car = pygame.transform.rotate(car, 90)
+        direction = 'x_right'
+        car_y += JUMP_VALUE
+        car_x += JUMP_VALUE
+        print('Turn left')
+        Left()
+        sleep(DELAY)
+        Forward()
+    
+    # turn to y up
+    if y_up == 255 and direction == 'x_right' and x_left == 255 and x_right == 255:
+        # turn from y_down
+        car = pygame.transform.rotate(car, 90)
+        direction = 'y_up'
+        car_y -= JUMP_VALUE + 5
+        car_x += JUMP_VALUE
+        print('Turn left')
+        Left()
+        sleep(DELAY)
+        Forward()
+    
+    # if car is stopped
+    if car_x == last_x and car_y == last_y:
+        # stop the engine sound
+        print("STOPPED")
+        Stop()
+        
+    pygame.display.update()  # update the window
+
+pygame.quit()      #close everything
