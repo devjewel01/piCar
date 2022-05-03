df7e46a2de39fb474b5acccf5eded94cfc9aa821
devjewel01 <devjewel01@stud.cou.ac.bd>
2022-05-03 12:11:24 +0600

    fixed motor.py

---
+++ virtualCar/motor.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+
+Motor1 = {'EN': 26, 'input1': 19, 'input2': 13}
+Motor2 = {'EN': 16, 'input1': 6, 'input2': 5}
+
+for x in Motor1:
+    GPIO.setup(Motor1[x], GPIO.OUT)
+    GPIO.setup(Motor2[x], GPIO.OUT)
+
+EN1 = GPIO.PWM(Motor1['EN'], 100)    
+EN2 = GPIO.PWM(Motor2['EN'], 100)    
+
+EN1.start(0)                    
+EN2.start(0)  
+
+
+def Run(a, b, c, d, x):
+    GPIO.output(Motor1['input1'], GPIO.LOW)
+    GPIO.output(Motor1['input2'], GPIO.LOW)
+    GPIO.output(Motor2['input1'], GPIO.LOW)
+    GPIO.output(Motor2['input2'], GPIO.LOW)
+
+    if a==1:
+        GPIO.output(Motor1['input1'], GPIO.HIGH)
+    if b==1:
+        GPIO.output(Motor1['input2'], GPIO.HIGH)
+    if c==1:
+        GPIO.output(Motor2['input1'], GPIO.HIGH)
+    if d==1:
+        GPIO.output(Motor2['input2'], GPIO.HIGH)
+
+    EN1.ChangeDutyCycle(x)
+    EN2.ChangeDutyCycle(x)
+    
+
+
+def Stop():
+    Run(0,0,0,0,0)
+
+def Start_Slow(a, b, c, d):
+    for i in range(0,100,20):
+        Run(a,b,c,d,i)
+        time.sleep(0.5)
+    
+def Stop_Slow(a,b,c,d):
+    for i in range(100,0,-20):
+        Run(a,b,c,d,i)
+        time.sleep(0.5)
+
+
+def Forward(x=50):
+    Run(1,0,1,0,x)
+
+def Backward(x=50):
+    Run(0,1,0,1,x)
+
+def Left(x=50):
+    Run(0,1,1,0,x)
+
+def Right(x=50):
+    Run(1,0,0,1,x)
+
+def Start():
+    Start_Slow(1,0,1,0)
+
+def StopSlowly():
+    Stop_Slow(1,0,1,0)
+
+
+
+
+    
+
+
+
+    
